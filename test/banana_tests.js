ec9e706673d37e2e828162830bf3397060e8c7cf
Whitney Young <wbyoung@fadingred.com>
2014-09-22 11:06:34 -0700

    Default values for banana property.

--- test/banana_tests.js
+++ test/banana_tests.js
@@ -6,5 +6,21 @@ var Banana = require('../lib/banana');
 describe('Banana', function() {
   it('can be created', function() {
     new Banana();
-  })
+  });
+
+  it('can be peeled');
+
+  describe('instance', function() {
+    beforeEach(function() {
+      this.banana = new Banana();
+    });
+
+    it('is edible', function() {
+      expect(this.banana.edible()).to.be.true;
+    });
+
+    it('is yellow', function() {
+      expect(this.banana.color()).to.eql('yellow');
+    });
+  });
 });
